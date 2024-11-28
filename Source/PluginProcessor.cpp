e187187bd6eb6841618474bcedfeb5d5673f48df
Kristian McGurran <81828472+McGurranK@users.noreply.github.com>
2024-11-28 19:25:29 +0000

    Removing and cleaning up project

--- Source/PluginProcessor.cpp
+++ Source/PluginProcessor.cpp
@@ -1,37 +1,19 @@
-/*
-  ==============================================================================
-
-    This file contains the basic framework code for a JUCE plugin processor.
-
-  ==============================================================================
-*/
-
 #include "PluginProcessor.h"
 #include "PluginEditor.h"
 
 //==============================================================================
 DelayPluginAudioProcessor::DelayPluginAudioProcessor()
-#ifndef JucePlugin_PreferredChannelConfigurations
-     : AudioProcessor (BusesProperties()
-                     #if ! JucePlugin_IsMidiEffect
-                      #if ! JucePlugin_IsSynth
-                       .withInput  ("Input",  juce::AudioChannelSet::stereo(), true)
-                      #endif
-                       .withOutput ("Output", juce::AudioChannelSet::stereo(), true)
-                     #endif
-                       )
-#endif
+     : AudioProcessor (BusesProperties().withInput  ("Input", juce::AudioChannelSet::stereo(), true).withOutput ("Output", juce::AudioChannelSet::stereo(), true))
 {
 }
 
 DelayPluginAudioProcessor::~DelayPluginAudioProcessor()
 {
-	// Clearing buffers when program existed
+
 	mDelayBufferLeft.clear();
 	mDelayBufferRight.clear();
 }
 
-//==============================================================================
 const juce::String DelayPluginAudioProcessor::getName() const
 {
     return JucePlugin_Name;
@@ -39,29 +21,17 @@ const juce::String DelayPluginAudioProcessor::getName() const
 
 bool DelayPluginAudioProcessor::acceptsMidi() const
 {
-   #if JucePlugin_WantsMidiInput
-    return true;
-   #else
     return false;
-   #endif
 }
 
 bool DelayPluginAudioProcessor::producesMidi() const
 {
-   #if JucePlugin_ProducesMidiOutput
-    return true;
-   #else
     return false;
-   #endif
 }
 
 bool DelayPluginAudioProcessor::isMidiEffect() const
 {
-   #if JucePlugin_IsMidiEffect
-    return true;
-   #else
     return false;
-   #endif
 }
 
 double DelayPluginAudioProcessor::getTailLengthSeconds() const
@@ -71,8 +41,7 @@ double DelayPluginAudioProcessor::getTailLengthSeconds() const
 
 int DelayPluginAudioProcessor::getNumPrograms()
 {
-    return 1;   // NB: some hosts don't cope very well if you tell them there are 0 programs,
-                // so this should be at least 1, even if you're not really implementing programs.
+    return 1;
 }
 
 int DelayPluginAudioProcessor::getCurrentProgram()
@@ -96,16 +65,10 @@ void DelayPluginAudioProcessor::changeProgramName (int index, const juce::String
 //==============================================================================
 void DelayPluginAudioProcessor::prepareToPlay (double sampleRate, int samplesPerBlock)
 {
-
-
-	// setting up delay buffer size for two seconds
-	mDelayBufferSize = (mDelayTime*sampleRate)*2;  	
+	mDelayBufferSize = (mDelayTime*sampleRate)*2;
 	
-	// Setting buffer size
 	mDelayBufferLeft.resize(mDelayBufferSize);
 	mDelayBufferRight.resize(mDelayBufferSize);
-
-
 }
 
 void DelayPluginAudioProcessor::releaseResources()
@@ -114,38 +77,23 @@ void DelayPluginAudioProcessor::releaseResources()
     // spare memory, etc.
 }
 
-#ifndef JucePlugin_PreferredChannelConfigurations
 bool DelayPluginAudioProcessor::isBusesLayoutSupported (const BusesLayout& layouts) const
 {
-  #if JucePlugin_IsMidiEffect
-    juce::ignoreUnused (layouts);
-    return true;
-  #else
-    // This is the place where you check if the layout is supported.
-    // In this template code we only support mono or stereo.
-    // Some plugin hosts, such as certain GarageBand versions, will only
-    // load plugins that support stereo bus layouts.
     if (layouts.getMainOutputChannelSet() != juce::AudioChannelSet::mono()
      && layouts.getMainOutputChannelSet() != juce::AudioChannelSet::stereo())
         return false;
 
-    // This checks if the input layout matches the output layout
-   #if ! JucePlugin_IsSynth
     if (layouts.getMainOutputChannelSet() != layouts.getMainInputChannelSet())
         return false;
-   #endif
-
+    
     return true;
-  #endif
 }
-#endif
 
 void DelayPluginAudioProcessor::processBlock (juce::AudioBuffer<float>& buffer, juce::MidiBuffer& midiMessages)
 {
     juce::ScopedNoDenormals noDenormals;
     auto totalNumInputChannels  = getTotalNumInputChannels();
     auto totalNumOutputChannels = getTotalNumOutputChannels();
-
 
     for (auto i = totalNumInputChannels; i < totalNumOutputChannels; ++i)
         buffer.clear (i, 0, buffer.getNumSamples());
@@ -219,18 +167,9 @@ void DelayPluginAudioProcessor::processBlock (juce::AudioBuffer<float>& buffer, 
 		// Iterate write and read position
 		mWritePosition++;
 		mReadPosition++;
-
-		
-
-	}
-
-
-
+}
 }
 
-
-
-//==============================================================================
 bool DelayPluginAudioProcessor::hasEditor() const
 {
     return false; // (change this to false if you choose to not supply an editor)
@@ -241,22 +180,15 @@ juce::AudioProcessorEditor* DelayPluginAudioProcessor::createEditor()
     return new DelayPluginAudioProcessorEditor (*this);
 }
 
-//==============================================================================
+
 void DelayPluginAudioProcessor::getStateInformation (juce::MemoryBlock& destData)
 {
-    // You should use this method to store your parameters in the memory block.
-    // You could do that either as raw data, or use the XML or ValueTree classes
-    // as intermediaries to make it easy to save and load complex data.
 }
 
 void DelayPluginAudioProcessor::setStateInformation (const void* data, int sizeInBytes)
 {
-    // You should use this method to restore your parameters from this memory block,
-    // whose contents will have been created by the getStateInformation() call.
 }
 
-//==============================================================================
-// This creates new instances of the plugin..
 juce::AudioProcessor* JUCE_CALLTYPE createPluginFilter()
 {
     return new DelayPluginAudioProcessor();
