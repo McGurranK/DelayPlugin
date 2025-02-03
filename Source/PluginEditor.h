b4995f22c037dc842ea87aa70796fa2d1bce69a0
Kristian McGurran <kmcgurran123@hotmail.com>
2025-02-03 19:30:38 +0000

    Custom GUI Component Shell

--- Source/PluginEditor.h
+++ Source/PluginEditor.h
@@ -7,6 +7,51 @@
 #include <JuceHeader.h>
 #include "PluginProcessor.h"
 
+// Class to draw the delay signal ontop of the dry signal over 4 seconds
+// Todo: steal my Fifo code from Atesh
+// Todo
+class CustomScrollingView final : public juce::Component
+{
+public:
+    CustomScrollingView (juce::RangedAudioParameter& MixControlParameter)
+    : mixControlAttachment (MixControlParameter, [this](float MixParameterValue) { mixParameterValue = MixParameterValue; })
+    , vblankAttachment (this, [this]{ updatePoints(); })
+    
+    {}
+    
+    float calculateXPosition()
+    {
+        return 0.f;
+    }
+    
+    float calculateYPosition()
+    {
+        return 0.f;
+    }
+    
+    void updatePoints()
+    {
+        repaint();
+    }
+    
+    void paint (juce::Graphics& GraphicsRef)
+    {
+        GraphicsRef.setColour (juce::Colours::purple.withAlpha ( 1.f - mixParameterValue));
+        GraphicsRef.fillPath (drySignalPath);
+        
+        GraphicsRef.setColour (juce::Colours::purple.withAlpha ( 1.f - mixParameterValue));
+        GraphicsRef.fillPath (wetSignalPath);
+    }
+    
+    float mixParameterValue { 1.f };
+    
+    juce::Path drySignalPath;
+    juce::Path wetSignalPath;
+    
+    juce::ParameterAttachment mixControlAttachment;
+    juce::VBlankAttachment vblankAttachment;
+};
+
 class DelayPluginAudioProcessorEditor : public juce::AudioProcessorEditor
 {
 public:
